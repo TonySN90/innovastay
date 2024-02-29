ebc9b4beac96e0e3ce976ffd8e318776297ebb6d
TonySN90 <tony.heider90@gmail.com>
2024-02-29 12:39:23 +0100

    worked out form

--- src/features/cabins/CreateCabinForm.tsx
+++ src/features/cabins/CreateCabinForm.tsx
@@ -8,26 +8,72 @@ function CreateCabinForm() {
     formState: { errors },
   } = useForm();
 
-  function onSubmit() {
+  function onSubmit(e) {
+    e.preventDefault();
     console.log("submit");
   }
 
   return (
     <>
       <form
-        className="sm:p-3 md:p-5 transition-all"
+        className="p-3 md:p-5 transition-all"
         onSubmit={handleSubmit(onSubmit)}
       >
         <h2 className="font-semibold text-lg mb-3">Neues Zimmer hinzufügen</h2>
         <FormRow
-          label="Cabin name"
+          label="Zimmername"
+          type="text"
           id="name"
-          register={register}
-          required="This field is required"
+          register={{ register, required: "Dieses Feld ist erforderlich" }}
           error={errors?.name?.message}
         />
-        <Button type="inverted" size="md" extras="mr-2" content="abbrechen" />
-        <Button type="standard" size="md" content="hinzufügen" />
+        <FormRow
+          label="Max. Personen"
+          type="number"
+          id="capacity"
+          register={{
+            register,
+            required: "Dieses Feld ist erforderlich",
+            min: {
+              value: 1,
+              message: "Es sollte  1 betragen",
+            },
+          }}
+          error={errors?.capacity?.message}
+        />
+        <FormRow
+          label="Regulärer Preis"
+          type="number"
+          id="price"
+          register={{ register, required: "Dieses Feld ist erforderlich" }}
+          error={errors?.price?.message}
+        />
+        <FormRow
+          label="Angebots-Preis"
+          type="number"
+          id="discount"
+          register={{ register, required: "Dieses Feld ist erforderlich" }}
+          error={errors?.discount?.message}
+        />
+        <FormRow
+          label="Beschreibung"
+          type="textarea"
+          id="description"
+          register={{ register, required: "Dieses Feld ist erforderlich" }}
+          error={errors?.description?.message}
+        />
+        {/* <FormRow
+          type="fileinput"
+          id="fileinput"
+          register={{ register, required: "Dieses Feld ist erforderlich" }}
+          error={errors?.fileinput?.message}
+
+          // Hier weitermachen
+        /> */}
+        <div className="w-[full] flex justify-center md:justify-end mt-4">
+          <Button type="inverted" size="md" extras="mr-2" content="abbrechen" />
+          <Button type="standard" size="md" content="hinzufügen" />
+        </div>
       </form>
     </>
   );
@@ -35,24 +81,57 @@ function CreateCabinForm() {
 
 export default CreateCabinForm;
 
-function FormRow({ label, id, register, error }) {
+function FormRow({ label, id, register, error, type }) {
   return (
-    <div className="border-b-2 border-indigo-100 min-w-[300px] md:min-w-[600px] transition-all flex py-3 mb-4 items-center">
-      <label className="mr-5" htmlFor={label}>
-        Zimmername
-      </label>
-      <div>
+    <div className="border-b-2 border-indigo-100 min-w-[300px] md:min-w-[650px] transition-all flex flex-col md:flex-row py-4 items-center">
+      <Label label={label} />
+      <Input id={id} type={type} reg={register} error={error} />
+    </div>
+  );
+}
+
+function Label({ label }) {
+  return (
+    <label className="w-full md:w-[200px]" htmlFor={label}>
+      {label}
+    </label>
+  );
+}
+
+function Input({ id, reg, error, type }) {
+  const { register, required, min } = reg;
+
+  return (
+    <div className="w-full">
+      {type === "text" || type === "number" ? (
         <input
-          className="md:mr-10 md:w-[200px] border border-gray-300 rounded-sm h-8 mb-1"
-          type="text"
+          className="md:mr-10 w-full md:w-[220px] border border-gray-300 rounded-sm h-9"
+          type={type}
           id={id}
           disabled={false}
           {...register(id, {
-            required: "This field is required",
+            required: required,
+            min: min,
           })}
         />
-        {error && <span className="text-red-500 text-md">{error}</span>}
-      </div>
+      ) : (
+        ""
+      )}
+
+      {type === "textarea" && (
+        <textarea
+          className="md:mr-10 w-full md:w-[220px] border border-gray-300 rounded-sm h-14 mb-1"
+          type={type}
+          id={id}
+          disabled={false}
+          {...register(id, {
+            required: required,
+            min: min,
+          })}
+        ></textarea>
+      )}
+
+      {error && <span className="text-red-500 text-md">{error}</span>}
     </div>
   );
 }
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -2,7 +2,7 @@ function Button({ onClick, type, size, content, extras = "" }) {
   const buttonTypes = {
     basics: "border-2 rounded-lg shadow-lg hover:shadow-xl transition-all",
     md: "px-3 h-10",
-    lg: "px-3 h-14",
+    lg: "px-3 h-12",
     standard:
       "text-gray-100 bg-indigo-600 border-indigo-600 hover:bg-indigo-700",
     inverted:
