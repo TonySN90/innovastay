acb95175a6732d0abebd41d8c69b542dcfa312eb
TonySN90 <tony.heider90@gmail.com>
2024-04-15 18:06:40 +0200

    update dashboard section - today´s concerns

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,18 +1,84 @@
 import { TbDoorEnter } from "react-icons/tb";
 import { formatDate } from "../utils/datesHelper";
+import { BsPeopleFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
   return (
     <>
       <DbHeader />
-      <DbSection
-        title={`Was heute anliegt - ${formatDate(new Date(Date.now()))}`}
-      >
-        <DbInfoCard title="Anreisen" color="bg-green-200 text-green-800" />
-        <DbInfoCard title="Abreisen" color="bg-yellow-200 text-yellow-800" />
+      <DbSection title={`Heute - ${formatDate(new Date(Date.now()))}`}>
         <DbInfoCard
+          id="arrival"
+          title="Anreisen"
+          rowContent={[
+            {
+              name: "Daniel Güntherino",
+              bookingId: "25",
+              cabin: "Zimmer 1",
+              nights: "7 Nächte",
+            },
+            {
+              name: "Daniel Güntherino",
+              bookingId: "25",
+              cabin: "Zimmer 1",
+              nights: "7 Nächte",
+            },
+            {
+              name: "Daniel Güntherino",
+              bookingId: "25",
+              cabin: "Zimmer 1",
+              nights: "7 Nächte",
+            },
+            {
+              name: "Daniel Güntherino",
+              bookingId: "25",
+              cabin: "Zimmer 1",
+              nights: "7 Nächte",
+            },
+          ]}
+        />
+        <DbInfoCard
+          id="departure"
+          title="Abreisen"
+          rowContent={[
+            {
+              name: "Daniela Güntherino",
+              bookingId: "25",
+              cabin: "Zimmer 1",
+              nights: "7 Nächte",
+            },
+            {
+              name: "Daniela Güntherino",
+              bookingId: "25",
+              cabin: "Zimmer 1",
+              nights: "7 Nächte",
+            },
+          ]}
+        />
+        <DbInfoCard
+          id="presentGuests"
           title="Gäste im Haus"
-          color="bg-indigo-200 text-indigo-800"
+          rowContent={[
+            {
+              name: "Danielo Güntherino",
+              bookingId: "25",
+              cabin: "Zimmer 1",
+              nights: "7 Nächte",
+            },
+            {
+              name: "Danielo Güntherino",
+              bookingId: "25",
+              cabin: "Zimmer 1",
+              nights: "7 Nächte",
+            },
+            {
+              name: "Danielo Güntherino",
+              bookingId: "25",
+              cabin: "Zimmer 1",
+              nights: "7 Nächte",
+            },
+          ]}
         />
       </DbSection>
     </>
@@ -40,34 +106,62 @@ function DbSection({
   title: string;
 }) {
   return (
-    <section className="min-w-[300px] p-8 bg-gray-50">
-      <h3 className="text-xl font-semibold mb-4">{title}</h3>
-      <div className="flex justify-between">{children}</div>
+    <section className="p-8 bg-gray-50 rounded-lg shadow-xl shadow-indigo-100">
+      <div className="flex items-center mb-4 justify-between">
+        <h3 className="text-xl font-semibold">{title}</h3>
+        <Link to="/bookings">
+          <p className="text-indigo-500 font-semibold">zu den Buchungen</p>
+        </Link>
+      </div>
+      <div className="flex justify-between flex-wrap gap-4 m-auto">
+        {children}
+      </div>
     </section>
   );
 }
 
-function DbInfoCard({ title, color }: { title: string; color: string }) {
+function DbInfoCard({ id, title, rowContent }: { id: string; title: string }) {
+  let border = "";
+  let backgroundColor = "";
+  let textColor = "";
+
+  if (id === "arrival") {
+    border = "border-green-200";
+    backgroundColor = "bg-green-200";
+    textColor = "text-green-800";
+  }
+  if (id === "departure") {
+    border = "border-yellow-200";
+    backgroundColor = "bg-yellow-200";
+    textColor = "text-yellow-800";
+  }
+  if (id === "presentGuests") {
+    border = " border-indigo-200";
+    backgroundColor = "bg-indigo-200";
+    textColor = "text-indigo-800";
+  }
+
   return (
-    <table className="w-[300px] border-b-4 border-green-200">
-      <thead className={`block rounded-t-lg py-2 px-4 ${color}`}>
+    <table
+      className={`w-full lg:min-w-[270px] lg:w-[30%] h-[75px] border-b-4 ${border}`}
+    >
+      <thead className={`block rounded-t-lg py-2 px-4 ${backgroundColor} `}>
         <tr>
           <th className="text-left h-8">{title}</th>
         </tr>
       </thead>
       <tbody className="bg-gray-50 shadow-lg">
-        <InfoCardRow
-          name="Daniel Güntherino"
-          bookingId="25"
-          cabin="Zimmer 1"
-          nights="7 Nächte"
-        />
-        <InfoCardRow
-          name="Daniel Güntherino"
-          bookingId="25"
-          cabin="Zimmer 1"
-          nights="7 Nächte"
-        />
+        {rowContent.map((el) => (
+          <InfoCardRow
+            name={el.name}
+            bookingId={el.bookingId}
+            cabin={el.cabin}
+            nights={el.nights}
+            textColor={textColor}
+            backgroundColor={backgroundColor}
+            key={Math.random()}
+          />
+        ))}
       </tbody>
     </table>
   );
@@ -78,33 +172,37 @@ function InfoCardRow({
   bookingId,
   cabin,
   nights,
+  textColor,
+  backgroundColor,
 }: {
   name: string;
   bookingId: string;
   cabin: string;
   nights: string;
+  textColor: string;
+  backgroundColor: string;
 }) {
   return (
-    <tr className="hover:bg-gray-100 border-b">
-      <td>
-        <div className="flex justify-between p-4 items-center">
-          <div className="flex flex-col">
-            <div className="flex gap-3">
-              <div>
-                <TbDoorEnter className="text-green-400 text-lg" />
-              </div>
-              <span className="font-semibold">{name}</span>
+    <tr className="">
+      <td className="flex justify-between p-4">
+        <div className="flex flex-col">
+          <div className="flex gap-3">
+            <div>
+              <BsPeopleFill className={`${textColor} text-lg`} />
             </div>
-            <div className="font text-sm">
-              <span className="mr-3">
-                #{bookingId} - {cabin}
-              </span>
-              <span> {nights}</span>
-            </div>
+            <span className="font-semibold">{name}</span>
           </div>
-          <div className="flex items-center justify-center h-10 w-10 bg-green-200 rounded-full">
-            <TbDoorEnter className="text-green-800 text-lg cursor-pointer" />
+          <div className="font text-sm">
+            <span className="mr-3">
+              #{bookingId} - {cabin}
+            </span>
+            <span> {nights}</span>
           </div>
+        </div>
+        <div
+          className={`flex items-center justify-center h-10 w-10 ${backgroundColor} rounded-full`}
+        >
+          <TbDoorEnter className={`${textColor} text-lg cursor-pointer`} />
         </div>
       </td>
     </tr>
